a1a0c540ab59d737ce9aa31d25404bcb576b6452
Phtoros <81822585+Phtoros@users.noreply.github.com>
2021-05-30 01:38:16 -0700

    Update main.cpp

--- hash/main.cpp
+++ hash/main.cpp
@@ -1,77 +1,45 @@
-#include <iostream>
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/files.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/sha.h>
+#include <fstream>
+#include <vector>
 #include <string>
-#include <cstdlib> //exit()
-#include <cstring> // strpy()
-#include <unistd.h> //close()
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <iostream>
+
 using namespace std;
-void Exception(const string & why, const int exitCode ) // Исключения (возможные ошибки)
+
+int main ()
 {
-    cout << "ErrorCode:"<<exitCode <<endl<< why << endl;
-    exit(exitCode);
+
+CryptoPP::SHA512 obj;
+
+cout «"Имя: " « obj.AlgorithmName() « endl;
+cout « "DigestSize:" « obj.DigestSize() « endl;
+cout « "BlockSize:" « obj.BlockSize() « endl;
+
+fstream file;
+string path = "/home/artem/hash";
+string str1, str2;
+file.open(path);
+if(!file.is_open()) {
+cout « "Ошибка! Файл не открыт!" « endl;
+return 1;
 }
-int main()
-{
+while(true) {
+getline(file,str1);
+if (file.fail())
+break;
+str2 += str1;
+}
+cout « "Содержимое файла: " « str2 « endl;
 
-    // структура с адресом нашей программы (клиента)
-    sockaddr_in * selfAddr = new (sockaddr_in);
-    selfAddr->sin_family = AF_INET; // интернет протокол IPv4
-    selfAddr->sin_port = 0;         // любой порт на усмотрение ОС
-    selfAddr->sin_addr.s_addr = 0;
+vector<byte> digest (obj.DigestSize());
 
-    // структура с адресом "на той стороне" (сервера)
-    sockaddr_in * remoteAddr = new (sockaddr_in);
-    remoteAddr->sin_family = AF_INET;     // интернет протокол IPv4
-    remoteAddr->sin_port = htons(7);  // port 7
-    remoteAddr->sin_addr.s_addr = inet_addr("82.179.90.12"); //  адрес
+obj.Update(reinterpret_cast<const byte*>(str2.data()),str2.size()); // формирование хэша
+obj.Final(digest.data()); // получаем хэш
 
-    // буфер для передачи и приема данных
-    char *buffer = new char[4096];
-    strcpy(buffer,"Hello, my name is Artem!");   //копируем строку
-    int msgLen = strlen(buffer);           //вычисляем длину строки
-
-    // создаём сокет
-    int mySocket = socket(AF_INET, SOCK_STREAM, 0); //tcp протокол
-    if (mySocket == -1) {
-        close(mySocket);
-        Exception("Error open socket",11);
-    }
-    //связываем сокет с адрессом
-    int rc = bind(mySocket,(const sockaddr *) selfAddr, sizeof(sockaddr_in));
-    if (rc == -1) {
-        close(mySocket);
-        Exception("Error bind socket with local address",12);
-    }
-
-    //установливаем соединение
-    rc = connect(mySocket, (const sockaddr*) remoteAddr, sizeof(sockaddr_in));
-    if (rc == -1) {
-        close(mySocket);
-        Exception("Error connect socket with remote server.", 13);
-    }
-
-    // передаём сообщение из буффера
-    rc = send(mySocket, buffer, msgLen, 0);
-    if (rc == -1) {
-        close(mySocket);
-        Exception("Error send message", 14);
-    }
-    cout << "We send: " << buffer << endl;
-
-    // принимаем ответ в буффер
-    rc = recv(mySocket, buffer, 4096, 0);
-    if (rc == -1) {
-        close(mySocket);
-        Exception("Error receive answer.", 15);
-    }
-    buffer[rc] = '\0'; // конец принятой строки
-    cout << "We receive: " << buffer << endl; // вывод полученного сообщения от сервера
-    // закрыем сокет
-    close(mySocket);
-
-    delete selfAddr;
-    delete remoteAddr;
-    delete[] buffer;
-    return 0;
+CryptoPP::StringSource(digest.data(),digest.size(),true, new CryptoPP::HexEncoder(new CryptoPP::FileSink(cout))); // вывод хэша в шестнадцатиричном формате
+cout « endl;
+return 0;
 }
